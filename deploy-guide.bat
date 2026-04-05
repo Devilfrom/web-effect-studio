a8b604de927b754a2b5439ea51031aaaba7516de
Devilfrom <dev@devilfrom.com>
2026-04-05 13:23:07 +0800

    Add deploy guide

---
+++ deploy-guide.bat
@@ -0,0 +1,15 @@
+@echo off
+echo ===== Web Effect Studio Vercel Deploy =====
+echo.
+echo This will deploy the project to Vercel
+echo.
+echo Steps:
+echo 1. Make sure you are logged into Vercel in your browser
+echo 2. Go to https://vercel.com/new
+echo 3. Click "Import Project" then "Upload"
+echo 4. Drag this folder into the browser:
+echo    C:\Users\Administrator\.qclaw\workspace\web-effect-studio\dist
+echo.
+echo After deploying, Vercel will give you a URL
+echo.
+pause
